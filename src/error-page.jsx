6b9af9c9a7bdd7bcc9e7fb36b5634834735ba784
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-10-26 19:34:04 +0700

    update error page

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,17 +1,30 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
 import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
-  const error = useRouteError(); 
+  const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <Container id="error-page" sx={{ height: "100vh" }}>
+      <Stack
+        alignItems="center"
+        justifyContent="center"
+        sx={{ height: "100%" }}
+      >
+        <Box>
+          <Typography variant="h1" marginBottom={3}>
+            Oops!
+          </Typography>
+          <Typography variant="p" textAlign="center" marginBottom={3}>
+            Sorry, an unexpected error has occurred.
+          </Typography>
+          <Typography textAlign="center" marginBottom={2}>
+            <i>{error.statusText || error.message}</i>
+          </Typography>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
